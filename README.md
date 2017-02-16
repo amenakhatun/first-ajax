7313a02e3473f3149501ca861881e006189b3baa
Amena <ame1.khatun@gmail.com>
2017-02-16 17:29:06 -0500

    created done faile and always functions

--- README.md
+++ README.md
@@ -23,3 +23,17 @@ time: 77ms
 status: 200
 initiator: other
 type: document
+
+part 3)
+ How do we take action in this success scenario?
+  use jquery to send a response back via a textbox or a notification message exp conole.log or alert box notifying the user that their click was successful
+ How do we make use of the data the server sends back?
+  the server will post the data requested to the user
+
+part 4) the new http status code is 500 internal server error
+
+part 7)
+no one beside me
+but every time I refereshed it went up by one... and then when I refreshed after 3 minutes it went up by 7
+
+part 8)
--- ajax.js
+++ ajax.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   $.ajax({
-    url: 'http://first-ajax-api.herokuapp.com',
+    url: 'http://first-ajax-api.herokuapp.com/',
     method: 'GET',
     data:{},
     dataType: 'text',
@@ -10,6 +10,32 @@ $(document).ready(function() {
   $("#Run-AJAX-Request-to-Root").on('click', function() {
       console.log('clicked');
   });
+
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/count',
+    method: 'GET',
+    data: {},
+    dataType: 'text',
+
+  }).done(function(data){
+    $('section').append(data);
+    console.log('Response Data');
+
+  }).fail(function(data){
+    $('section').append(data);
+    console.log('will try harder next time!');
+
+  }).always(function(data){
+    console.log('Hey The Request finsihed!')
+  });
+
+  $("#Run-AJAX-Request-to-Ping-Pong").on('click', function() {
+      console.log('clicked');
+  });
+
+//Also in the done callback use jQuery to append
+//the responseData string to the <section> element.
+
 
 });
 
