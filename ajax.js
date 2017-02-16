d721de631bfe4a6102e157dfcd8f0fb91db7ad11
Amena <ame1.khatun@gmail.com>
2017-02-16 18:08:38 -0500

    finsihed first-ajax assignment

--- ajax.js
+++ ajax.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   $.ajax({
-    url: 'http://first-ajax-api.herokuapp.com/',
+    url: 'http://first-ajax-api.herokuapp.com/ping',
     method: 'GET',
     data:{},
     dataType: 'text',
@@ -21,6 +21,7 @@ $(document).ready(function() {
     $('section').append(data);
     console.log('Response Data');
 
+
   }).fail(function(data){
     $('section').append(data);
     console.log('will try harder next time!');
@@ -32,6 +33,30 @@ $(document).ready(function() {
   $("#Run-AJAX-Request-to-Ping-Pong").on('click', function() {
       console.log('clicked');
   });
+
+
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/time',
+    method: 'GET',
+    data:'Europe/Sofia',
+    dataType: 'text',
+  }).done(function(step8){
+    $('section').append(step8);
+    console.log('Response Data');
+  });
+
+
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/a_car',
+      method: 'GET',
+      data: {},
+      dataType: 'html',
+    }).done(function(step9){
+      $('#car').append(step9);
+      console.log('Response Data');
+    });
+
+
 
 //Also in the done callback use jQuery to append
 //the responseData string to the <section> element.
