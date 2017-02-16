39bf675ba2e31838303863dc680f700443e156a2
Amena <ame1.khatun@gmail.com>
2017-02-16 15:54:25 -0500

    created a click event in jquery and a button on html

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Your First (Several) Asynchronous Requests
 
 [See assignment in Alexa.](https://alexa.bitmaker.co/cohorts/67/assignments/2055/latest)
+
+Part 0
+method: get
+time: 50ms
+status: 200
+initiator: 1
+type: document
+
+part 1
+
+1) http server
+2) POST request
+3)
+4) HTML
+
+
+practice:
+method: get
+time: 77ms
+status: 200
+initiator: other
+type: document
--- ajax.js
+++ ajax.js
@@ -1,5 +1,21 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-  /* Your code goes here */
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com',
+    method: 'GET',
+    data:{},
+    dataType: 'text',
+  });
+
+  $("#Run-AJAX-Request-to-Root").on('click', function() {
+      console.log('clicked');
+  });
 
 });
+
+
+// Back in ajax.js, Create a 'click' event handler for
+// the button, and move your AJAX call inside it.
+// Refresh your page to load the new JavaScript.
+// Try clicking your button a few times! In your network tab, inspect the requests as they come in.
+// Now we are in control of when the request is made.
